0a14a3a985c5d7b3b84c0e32e050dd13a0bc300f
wladyslaw.majdanik <wladyslaw.majdanik@gmail.com>
2018-03-01 21:06:10 +0100

    first commit

---
+++ app/src/main/java/pl/dailyveryapp/RestaurantListActivity.java
@@ -0,0 +1,13 @@
+package pl.dailyveryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restraurant_list);
+    }
+}
